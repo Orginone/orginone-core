34d753aaaf9f910d9ef27ec03abd613aa6ec92df
15168347908 <296778233@qq.com>
2023-06-14 16:27:51 +0800

    fix: 合并推送

--- packages/core/src/lib/domain/target/cohort/CohortModel.ts
+++ packages/core/src/lib/domain/target/cohort/CohortModel.ts
@@ -1,8 +1,50 @@
+import { service } from "@/di";
 import { XTarget } from "@/lib/base/schema";
 import { CollectionImpl } from "@/lib/model/ModelContext";
+import CohortService from "./CohortService";
+import { IState, StateAction, Store } from "@/state";
+import { UserStore } from "@/lib/store/user";
 
+@service(["StateAction", "UserStore", CohortService])
 export default class CohortModel extends CollectionImpl<XTarget> {
+  private readonly service: CohortService;
+  private readonly userStore: Store<UserStore>;
 
-    
+  constructor(
+    stateAction: StateAction,
+    userStore: Store<UserStore>,
+    service: CohortService
+  ) {
+    super();
+    this.userStore = userStore;
+    this.service = service;
+    this._cohorts = stateAction.create(this.collection);
+  }
 
+  private _cohorts: IState<XTarget[]>;
+  private _cohortLoaded: boolean = false;
+
+  get cohorts(): XTarget[] {
+    return this._cohorts.value;
+  }
+
+  TargetCohorts(targetId: string): XTarget[] {
+    return this._cohorts;
+  }
+
+  get userId(): string {
+    return this.userStore.currentUser.value.id;
+  }
+
+  async loadUserCohort(reload?: boolean | undefined): Promise<XTarget[]> {
+    if (!this._cohortLoaded || reload) {
+      const res = await this.service.queryCohorts(this.userId);
+      if (res.success) {
+        this._cohortLoaded = true;
+
+        this._cohorts.value = res.data.result ?? [];
+      }
+    }
+    return this.cohorts;
+  }
 }
--- packages/core/src/lib/domain/target/company/CompanyModel.ts
+++ packages/core/src/lib/domain/target/company/CompanyModel.ts
@@ -1,14 +1,45 @@
 import { service } from "@/di";
 import { XTarget } from "@/lib/base/schema";
 import { CollectionImpl } from "@/lib/model/ModelContext";
-import { IState, StateAction } from "@/state";
+import { IState, StateAction, Store } from "@/state";
+import CompanyService from "./CompanyService";
+import { UserStore } from "@/lib/store/user";
 
-@service(["StateAction"])
+@service(["StateAction", "UserStore", CompanyService])
 export default class CompanyModel extends CollectionImpl<XTarget> {
-  private _companies: IState<XTarget[]>;
+  private readonly service: CompanyService;
+  private readonly userStore: Store<UserStore>;
 
-  constructor(stateAction: StateAction){
-    super()
-    this._companies = stateAction.create([]);
+  constructor(
+    stateAction: StateAction,
+    userStore: Store<UserStore>,
+    service: CompanyService
+  ) {
+    super();
+    this.userStore = userStore;
+    this.service = service;
+    this._companies = stateAction.create(this.collection);
+  }
+
+  private _companies: IState<XTarget[]>;
+  private _companyLoaded: boolean = false;
+
+  get companies(): XTarget[] {
+    return this._companies.value;
+  }
+
+  get userId(): string {
+    return this.userStore.currentUser.value.id;
+  }
+
+  async loadCompanies(reload?: boolean | undefined): Promise<XTarget[]> {
+    if (!this._companyLoaded || reload) {
+      const res = await this.service.queryCompanies(this.userId);
+      if (res.success) {
+        this._companyLoaded = true;
+        await this.createModel(res.data.result ?? []);
+      }
+    }
+    return this.companies;
   }
 }
